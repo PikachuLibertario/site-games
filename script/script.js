1b6eeab9281f17001e9f76c4326a59972d4f9532
lendomanga <diafrio99@gmail.com>
2022-10-01 00:54:35 -0300

    site 2.0

--- script/script.js
+++ script/script.js
@@ -1,19 +1,45 @@
 function pesquisar(){
-    let entrada,filtro,jogos,jogosItens,links;
+    let entrada,filtro,jogos,jogosItens,links,x,y;
 
     entrada = document.getElementById("pesquisa");
     filtro = entrada.value.toUpperCase();
     jogos = document.getElementById("jogos");
     jogosItens = jogos.getElementsByTagName("li");
 
-    for(let i=0; i < jogosItens.length;i++){
-        links = jogosItens[i].getElementsByTagName("a")[0];
+    x = document.getElementById("nao-achado");
+    y = 0;
 
-        if(links.innerHTML.toUpperCase().indexOf(filtro) > -1){
-            jogosItens[i].style.display="block"; /*mostra o elemento*/
+    if(filtro != ""){
+        for(let i=0; i < jogosItens.length;i++){
+            links = jogosItens[i].getElementsByTagName("a")[0];
+
+            if(links.innerHTML.toUpperCase().indexOf(filtro) > -1){
+                jogos.style.display="block";
+                jogosItens[i].style.display="block"; /*mostra o elemento*/
+                y++;
+            }
+            else{
+                jogosItens[i].style.display="none";
+            }
+        }
+        if(y >= 1){
+            x.style.display="none";
         }
         else{
-            jogosItens[i].style.display="none";
+            x.style.display="block";
         }
     }
+}
+function limpar(){
+    var a = document.getElementById("pesquisa");
+    a.value='';
+
+    var b = document.getElementById("nao-achado");
+    b.style.display="none";
+
+    var c = document.getElementById("jogos");
+
+    c.style.display="none";
+
+    a.focus();
 }
